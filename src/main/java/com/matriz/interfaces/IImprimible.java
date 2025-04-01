b878dcdf49f58410708b0302803f9cb2c426ce9d
UnaxPCgrande <allerunax@gmail.com>
2025-04-01 19:26:30 +0200

    interfaces

--- src/main/java/com/matriz/interfaces/IImprimible.java
+++ src/main/java/com/matriz/interfaces/IImprimible.java
@@ -1,4 +1,13 @@
 package com.matriz.interfaces;
 
-public class IImprimible {
+/**
+ * Interfaz que define las operaciones de impresión de una matriz.
+ * Siguiendo el Principio de Segregación de Interfaces (I de SOLID).
+ */
+public interface IImprimible {
+    /**
+     * Imprime la matriz en formato legible.
+     * @return Representación en cadena de texto de la matriz.
+     */
+    String imprimir();
 }
--- src/main/java/com/matriz/interfaces/ITransponible.java
+++ src/main/java/com/matriz/interfaces/ITransponible.java
@@ -1,4 +1,13 @@
 package com.matriz.interfaces;
 
-public class ITransponible {
+/**
+ * Interfaz que define la operación de transpuesta.
+ * Siguiendo el Principio de Segregación de Interfaces (I de SOLID).
+ */
+public interface ITransponible<T> {
+    /**
+     * Calcula la transpuesta de la matriz.
+     * @return Una nueva instancia que representa la transpuesta.
+     */
+    T transpuesta();
 }
